58e0c1e35d7597195e8e5ffe63d66b67bf334a8e
WikyPassu <47321306+WikyPassu@users.noreply.github.com>
2019-02-25 11:05:10 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,34 +1,34 @@
 function Mostrar() {
-	var numero,respuesta,sumPos = 0,sumNeg = 0,cantPos = 0,cantNeg = 0,cantCeros = 0,cantPar = 0,promPos,promNeg,diferencia;
-	do{
+	var numero, respuesta, sumPos = 0, sumNeg = 0, cantPos = 0, cantNeg = 0, cantCeros = 0, cantPar = 0, promPos, promNeg, diferencia;
+	do {
 		numero = parseInt(prompt("Ingrese un número:"));
-		while(isNaN(numero)){
+		while (isNaN(numero)) {
 			numero = parseInt(prompt("Número inválido. Ingrese un número:"));
 		}
-		if(numero > 0){
+		if (numero > 0) {
 			cantPos++;
 			sumPos += numero;
 		}
-		else if(numero < 0){
+		else if (numero < 0) {
 			cantNeg++;
 			sumNeg += numero;
 		}
-		else{
+		else {
 			cantCeros++;
 		}
-		if(numero % 2 == 0){
+		if (numero % 2 == 0) {
 			cantPar++;
 		}
 		respuesta = prompt("¿Desea seguir ingresando números?").toLowerCase();
-	}while(respuesta == "s");
+	} while (respuesta == "s");
 	promPos = sumPos / cantPos;
 	promNeg = sumNeg / cantNeg;
-	if(isNaN(promPos)){
+	if (isNaN(promPos)) {
 		promPos = 0;
 	}
-	if(isNaN(promNeg)){
+	if (isNaN(promNeg)) {
 		promNeg = 0;
 	}
 	diferencia = cantPos - cantNeg;
-	document.write("Suma de positivos: "+sumPos+"<br>Suma de negativos: "+sumNeg+"<br>Cantidad de positivos: "+cantPos+"<br>Cantidad de negativos: "+cantNeg+"<br>Cantidad de ceros: "+cantCeros+"<br>Cantidad de numeros pares: "+cantPar+"<br>Promedio de positivos: "+promPos+"<br>Promedio de negativos: "+promNeg+"<br>Diferencia entre positivos y negativos: "+diferencia);
+	document.write("Suma de positivos: " + sumPos + "<br>Suma de negativos: " + sumNeg + "<br>Cantidad de positivos: " + cantPos + "<br>Cantidad de negativos: " + cantNeg + "<br>Cantidad de ceros: " + cantCeros + "<br>Cantidad de numeros pares: " + cantPar + "<br>Promedio de positivos: " + promPos + "<br>Promedio de negativos: " + promNeg + "<br>Diferencia entre positivos y negativos: " + diferencia);
 }
