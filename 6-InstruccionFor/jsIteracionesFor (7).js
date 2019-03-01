77e883107b6eff6e9e3885615748100853ebbc9a
WikyPassu <47321306+WikyPassu@users.noreply.github.com>
2019-03-01 09:55:07 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -12,7 +12,11 @@ function Mostrar() {
     }
     console.log("Cantidad de numeros divisores encontrados: "+cont);
 }
-/* Si no quiero contar al numero ingresado como divisor,
+/* La idea es ir iterando y dividiendo al numero por
+la variable de control, luego compruebo si el resto
+de esa operacion da 0 (es decir, es divisible por ese
+numero) => cont++.
+Si no quiero contar al numero ingresado como divisor,
 debo comprobar los valores que hay desde el 1 hasta la
 mitad del numero del que quiero sacar sus divisores, 
 porque mas de la mitad, nunca voy a encontrar ninguno
--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -16,3 +16,13 @@ function Mostrar() {
         alert("El numero "+numero+" no es primo.");
     }
 }
+/* La idea es ir iterando y dividiendo al numero por
+la variable de control, luego compruebo si el resto
+de esa operacion da 0 (es decir, es divisible por ese
+numero) => cont++.
+Yo busco que mi cont == 2 porque un numero primo es
+aquel numero natural > 1 que solo es divisible por
+1 y sí mismo. Si esta condicion no se cumple, mi cont
+habra sido incrementado numerosas veces porque el
+numero ingresado tiene mas de 2 divisores. 
+*/
