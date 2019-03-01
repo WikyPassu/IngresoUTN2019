6762d1a771298ddeb526078a82fd332c6ef7c61d
WikyPassu <47321306+WikyPassu@users.noreply.github.com>
2019-03-01 12:26:02 -0300

    Add files via upload

--- ExamenViejo/codigoFuenteParcial/7-iteraciones.js
+++ ExamenViejo/codigoFuenteParcial/7-iteraciones.js
@@ -1,6 +1,6 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar() {
-    var promedio, suma = 0, notaBaja, menor, alumno, contVar = 0, flag = 0;
+    var promedio, suma = 0, notaBaja, menor, alumno, sexoBajo, contVar = 0, flag = 0;
     for (i = 1; i <= 6; i++) {
         var nota = parseInt(prompt("Alumno " + i + "- Ingrese la nota:"));
         while (isNaN(nota) || nota < 0 || nota > 10) {
@@ -13,6 +13,7 @@ function Mostrar() {
         if (nota < menor || flag == 0) {
             menor = nota;
             alumno = i;
+            sexoBajo = sexo;
             flag = 1;
         }
         if (nota >= 6 && sexo == "m") {
@@ -21,6 +22,6 @@ function Mostrar() {
         suma += nota;
     }
     promedio = suma / 6;
-    alert("Promedio de notas total: " + promedio.toFixed(2) + "\nNota más baja: " + menor + " del alumno" + alumno + "\nCantidad de varones cuya nota ha sido mayor o igual a 6: " + contVar);
+    alert("a) Promedio de notas total: " + promedio.toFixed(2) + "\nb) La nota más baja fue un " + menor + " del Alumno" + alumno + " cuyo sexo es " + sexoBajo + "\nc) Cantidad de varones cuya nota ha sido mayor o igual a 6: " + contVar);
 }
 
--- ExamenViejo/codigoFuenteParcial/8-iteraciones.js
+++ ExamenViejo/codigoFuenteParcial/8-iteraciones.js
@@ -1,8 +1,28 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar() {
+    var pares = 0, promedio, cont = 0, suma = 0, resp, max, min, flag = 0;
+    do {
+        var numero = parseInt(prompt("Ingrese un numero positivo: "));
+        while (isNaN(numero) || numero < 0) {
+            numero = parseInt(prompt("Error. Ingrese un numero positivo: "));
+        }
 
-
-	
-
+        if (numero % 2 == 0) {
+            pares++;
+        }
+        if (numero > max || flag == 0) {
+            max = numero;
+        }
+        if (numero < min || flag == 0) {
+            min = numero;
+            flag = 1;
+        }
+        suma += numero;
+        cont++;
+        resp = confirm("¿Desea seguir ingresando numeros?");
+    } while (resp);
+    promedio = suma / cont;
+    document.write("a) Cantidad de numeros pares: " + pares + "<br>b) Promedio de todos los numeros: " + promedio.toFixed(2) + "<br>c) Suma de todos los numeros: " + suma + "<br>d) Numero maximo y minimo: " + max + " y " + min + " respetivamente.");
 }
+
 
